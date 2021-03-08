6b836e12ce5be350332adb337bdf1611b5e62249
suhyyyuuunnn <49545423+suhyyyuuunnn@users.noreply.github.com>
2021-03-08 19:13:14 +0900

    suhyun

--- posts/urls.py
+++ posts/urls.py
@@ -1,8 +1,21 @@
 from django.urls import path
-from .views import index, history
+from .views import index, history, chart, location, establish, market, register, find, information, jv, notice, news, reservation, about
+
 
 app_name = "posts"
 urlpatterns = [
     path('index/', index, name="index"),
     path('history/', history, name="history"),
+    path('chart/', chart, name="chart"),
+    path('location/', location, name="location"),
+    path('establish/', establish, name="establish"),
+    path('market/', market, name="market"),
+    path('register/', register, name="register"),
+    path('find/', find, name="find"),
+    path('information/', information, name="information"),
+    path('jv/', jv, name="jv"),
+    path('notice/', notice, name="notice"),
+    path('news/', news, name="news"),
+    path('reservation/', reservation, name="reservation"),
+    path('about/', about, name="about"),
 ]
--- posts/views.py
+++ posts/views.py
@@ -7,3 +7,51 @@ def index(request):
 
 def history(request):
     return render(request, 'posts/history.html')
+
+
+def chart(request):
+    return render(request, 'posts/chart.html')
+
+
+def location(request):
+    return render(request, 'posts/location.html')
+
+
+def establish(request):
+    return render(request, 'posts/consulting_establish.html')
+
+
+def market(request):
+    return render(request, 'posts/market.html')
+
+
+def register(request):
+    return render(request, 'posts/consulting_register.html')
+
+
+def find(request):
+    return render(request, 'posts/find.html')
+
+
+def information(request):
+    return render(request, 'posts/information.html')
+
+
+def jv(request):
+    return render(request, 'posts/jv.html')
+
+
+def notice(request):
+    return render(request, 'posts/notice.html')
+
+
+def news(request):
+    return render(request, 'posts/news.html')
+
+
+def reservation(request):
+    return render(request, 'posts/reservation.html')
+
+
+def about(request):
+    return render(request, 'posts/about.html')
